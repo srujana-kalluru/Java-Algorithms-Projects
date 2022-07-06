853211d3dfc1256ff192598669241965bf959f6c
creationk <creationk@users.noreply.github.com>
2022-07-06 15:51:02 +0530

    Initial commit

---
+++ src/test/java/com/BinarySearchTreeTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Slf4j
+class AppTest {
+    @Test
+    void test() {
+        fail("Not implemented");
+    }
+}
